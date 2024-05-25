baf6189afb517674f6b90d9b598b28ed94cc2d4a
Kyrylo Dorozhynskyi <kyrylo.dorozhynskyi@justdev.org>
2024-05-25 17:57:08 +0200

    [DEV] Add install scripts

--- wp-content/themes/template/functions.php
+++ wp-content/themes/template/functions.php
@@ -50,6 +50,12 @@ new JDEV\ACFConfig();
 
 // Models
 new JDEV\Model\Search();
-// new JDEV\Model\GravityForm();
+// if (!class_exists('GFAPI')) {
+// 	add_action('admin_notices', function () {
+// 		echo '<div class="error"><p>GravityForm not activated. Make sure you activate the plugin</p></div>';
+// 	});
+// } else {
+// 	new JDEV\Model\GravityForm();
+// }
 // new JDEV\Model\Person();
 // new JDEV\Model\News();
